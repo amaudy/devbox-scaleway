4f7780dc413f481fde83691e8bb6d0466120aa55
audy <amaudy@gmail.com>
2023-02-04 11:46:47 +0700

    Added Ansible for server provisioning

--- instance.tf
+++ instance.tf
@@ -7,6 +7,22 @@ resource "scaleway_instance_server" "devbox" {
   ip_id = scaleway_instance_ip.public_ip.id
 
   security_group_id = scaleway_instance_security_group.devbox.id
+
+  provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install python3 -y", "echo Ready to continue"]
+
+    connection {
+      host  = self.public_ip
+      type  = "ssh"
+      user  = "root"
+      agent = "true"
+    }
+  }
+
+  provisioner "local-exec" {
+    working_dir = "${path.cwd}/ansible"
+    command     = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.public_ip},'  setup-devbox.yml"
+  }
 }
 
 output "public_ip" {
